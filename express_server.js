6437a79fbc3609c3ee5436cf94ea6aa9973d2a46
Jonks <anastasiablackhold@gmail.com>
2021-02-15 21:48:11 +0000

    added new route to /urls for testing purposes

--- express_server.js
+++ express_server.js
@@ -16,6 +16,11 @@ app.get('/urls', (request, response) => {
   response.render('urls_index', templateVars);
 });
 
+app.get('/urls/:shortURL', (request, response) => {
+  const templateVars = { shortURL: request.params.shortURL, longURL: request.params.longURL };
+  response.render('urls_show', templateVars);
+});
+
 
 
 //******THE FOLLOWING IS FOR TESTING PURPOSES:
