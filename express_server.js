207ceb2bb8ffe73e9a63f97d319728aa96b429e2
Jonks <anastasiablackhold@gmail.com>
2021-02-18 20:48:32 +0000

    Added cookie-session to dependency list. Cookies are now encrypted.

--- express_server.js
+++ express_server.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const app = express();
 const PORT = 8080;
-const bodyParser = require("body-parser");
-const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const cookieSession = require('cookie-session');
 const bcrypt = require('bcrypt');
 const { urlDatabase, userDatabase, generateRandomString, checkEmail, urlPairs, urlOwner } = require('./helpers');
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(cookieParser());
+app.use(cookieSession({
+  name: 'session',
+  keys: ['key1', 'key2']
+}));
+
 
 
 //********************* WEBSITE PAGES: *********************
@@ -20,9 +24,9 @@ app.get('/', (request, response) => {
 
 //URL page - lists default database
 app.get('/urls', (request, response) => {
-  if (request.cookies.user_id) {
-    const user = JSON.parse(request.cookies.user_id);
-    const templateVars = {  user_id: request.cookies.user_id, urls: urlPairs(user.id) };
+  if (request.session.user_id) {
+    const user = JSON.parse(request.session.user_id);
+    const templateVars = {  user_id: request.session.user_id, urls: urlPairs(user.id) };
     response.render('urls_index', templateVars);
   } else {
     response.redirect('/login');
@@ -31,8 +35,8 @@ app.get('/urls', (request, response) => {
 
 //page for adding URLs to database
 app.get('/urls/new', (request, response) => {
-  const templateVars = { user_id: request.cookies.user_id };
-  if (request.cookies.user_id) {
+  const templateVars = { user_id: request.session.user_id };
+  if (request.session.user_id) {
     response.render('urls_new', templateVars);
   } else {
     response.redirect('/login');
@@ -41,19 +45,19 @@ app.get('/urls/new', (request, response) => {
 
 //registration page
 app.get('/register', (request, response) => {
-  const templateVars = { user_id: request.cookies.user_id };
+  const templateVars = { user_id: request.session.user_id };
   response.render('register', templateVars);
 });
 
 //login page
 app.get('/login', (request, response) => {
-  const templateVars = { user_id: request.cookies.user_id };
+  const templateVars = { user_id: request.session.user_id };
   response.render('login', templateVars);
 });
 
 //404 custom page
 app.get('/404', (request, response) => {
-  const templateVars = { user_id: request.cookies.user_id };
+  const templateVars = { user_id: request.session.user_id };
   response.render('404', templateVars);
 });
 
@@ -62,7 +66,7 @@ app.get('/404', (request, response) => {
 //add new URL to database
 app.post('/urls', (request, response) => {
   let shortURL = generateRandomString();
-  const user = JSON.parse(request.cookies.user_id);
+  const user = JSON.parse(request.session.user_id);
   urlDatabase[shortURL] = { longURL: request.body.longURL, owner: user.id };
   console.log(urlDatabase);
   response.redirect(`/urls/${shortURL}`);
@@ -71,10 +75,10 @@ app.post('/urls', (request, response) => {
 //delete URLs from database
 app.post('/urls/:shortURL/delete', (request, response) => {
 
-  const user = JSON.parse(request.cookies.user_id);
+  const user = JSON.parse(request.session.user_id);
   const shortURL = request.params.shortURL;
 
-  if (request.cookies.user_id) {
+  if (request.session.user_id) {
     const owner = urlOwner(user.id);
     console.log(owner[shortURL]);
     if (user.id === owner[shortURL]) {
@@ -92,7 +96,7 @@ app.post('/urls/:shortURL/delete', (request, response) => {
 //edit URLs from the database
 app.post('/urls/:shortURL', (request, response) => {
   const redirURL = request.params.shortURL;
-  const user = JSON.parse(request.cookies.user_id);
+  const user = JSON.parse(request.session.user_id);
   urlDatabase[redirURL] = { longURL: request.body.longURL, owner: user.id };
   response.redirect(`/urls/${redirURL}`);
 });
@@ -107,17 +111,22 @@ app.post('/register', (request, response) => {
   const id = request.body.id;
   const email = request.body.email;
   const password = request.body.password;
-  const hashedPassword = bcrypt.hashSync(password, 10);
   const eCheck = checkEmail(email);
+
 
   if (userDatabase[id] || eCheck) {
     response.status(400).send(`The username or email that you've provided already exists for us! Please try a different one!`);
   } else {
-    userDatabase[id] = { id: id, email: email, password: hashedPassword };
-    console.log(userDatabase);
-    const cookieUser = JSON.stringify(request.body);
-    response.cookie('user_id', cookieUser);
-    response.redirect('/urls');
+
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(password, salt, (err, hash) => {
+        userDatabase[id] = { id: id, email: email, password: hash };
+        console.log(userDatabase);
+        const cookieUser = JSON.stringify(request.body);
+        request.session.user_id = cookieUser;
+        response.redirect('/urls');
+      });
+    });
   }
 });
 
@@ -125,16 +134,18 @@ app.post('/register', (request, response) => {
 app.post('/login', (request, response) => {
   const email = request.body.email;
   const password = request.body.password;
-
   const user = checkEmail(email);
   
   if (user) {
-    if (email === userDatabase[user].email && bcrypt.compareSync(password, userDatabase[user].password)) {
-      response.cookie('user_id', JSON.stringify(userDatabase[user]));
-      response.redirect('/urls');
-    } else if (email === userDatabase[user].email) {
-      response.status(400).send(`Wrong password!`);
-    }
+    bcrypt.compare(password, userDatabase[user].password, (err, result) => {
+      if (email === userDatabase[user].email && result) {
+        request.session.user_id = JSON.stringify(userDatabase[user]);
+        response.redirect('/urls');
+      } else if (email === userDatabase[user].email) {
+        response.status(400).send(`Wrong password!`);
+      }
+    });
+
   } else {
     response.status(403).send(`User does not exist!`);
   }
@@ -142,7 +153,7 @@ app.post('/login', (request, response) => {
 
 //Logout (delete cookie)
 app.post('/logout', (request, response) => {
-  response.clearCookie('user_id');
+  delete request.session.user_id;
   response.redirect('/urls');
 });
 
@@ -151,15 +162,15 @@ app.post('/logout', (request, response) => {
 
 //urls route
 app.get('/urls/:shortURL', (request, response) => {
-  const user = JSON.parse(request.cookies.user_id);
+  const user = JSON.parse(request.session.user_id);
   const urls = urlPairs(user.id);
   const shortURL = request.params.shortURL;
 
-  if (request.cookies.user_id) {
+  if (request.sesion.user_id) {
     const owner = urlOwner(user.id);
     console.log(owner[shortURL]);
     if (user.id === owner[shortURL]) {
-      const templateVars = {  user_id: request.cookies.user_id, shortURL: shortURL, longURL: urls[shortURL] };
+      const templateVars = {  user_id: request.session.user_id, shortURL: shortURL, longURL: urls[shortURL] };
       response.render('urls_show', templateVars);
     } else {
       response.status(404).send(`Uh oh! That Smol-Link does not exist!`);
